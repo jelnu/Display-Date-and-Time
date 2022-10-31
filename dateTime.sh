b55e9121ed52e840ce33be9f9d2664f42a612cc1
jelnu <101507938+jelnu@users.noreply.github.com>
2022-10-31 06:25:44 -0400

    Add files via upload

---
+++ dateTime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Current Date and Time, please wait..."
+now="$(date)"
+printf "Current date and time %s\n" "$now"
+ 
+now="$(date +'%d/%m/%Y')"
+printf "Current date in dd/mm/yyyy format %s\n" "$now"
+ 
